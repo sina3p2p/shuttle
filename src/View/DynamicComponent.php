ef1b9aec30801b65ea705a764d19ce43c548b36c
Sina <sinabehzadi2014@gmail.com>
2022-07-28 01:50:34 +0400

    bug fix

--- src/View/DynamicComponent.php
+++ src/View/DynamicComponent.php
@@ -15,7 +15,7 @@ class DynamicComponent extends Component
 
     protected string $view;
 
-    public function __construct($name, $data = null, $modelAutoLoad = true)
+    public function __construct($name, $data = null, $model = null, $modelSetting = null, $modelAutoLoad = true)
     {
         $this->view = "components." . $name;
         $data = $data ?? [];
@@ -38,6 +38,34 @@ class DynamicComponent extends Component
                             $data[$row->details->column] = [];
                         }
                     });
+            }
+
+            if ($modelSetting && $model) {
+                $model = app($model);
+                //                foreach (data_get($modelSetting,'model.conditions',[]) as $con){
+                //                    $data = $data->{$con['type']}($con['field'], data_get($setting,$con['value'],$con['type'] == 'whereIn' ? [] : $con['value']));
+                //                }
+                //                $data = $data->with(data_get($this->model_settings,'model.relations',[]));
+                $pag = data_get($modelSetting, 'model.limit', -1);
+                //                if(data_get($modelSetting,'model.scope',false))
+                //                {
+                //                    // dd(data_get($this->component->model_settings,'model.scope'));
+                //                    $data = $data->{data_get($this->model_settings,'model.scope')}();
+                //                }
+                switch ($pag) {
+                    case -1:
+                        $data['model'] = $model->orderBy($model->getTable() . ".created_at")->get();
+                        break;
+                    case 0:
+                        $data['model'] = $model->first();
+                        //                        if($data && method_exists($model,'views')){
+                        //                            views($data)->record();
+                        //                        }
+                        break;
+                    default:
+                        $data['model'] = $model->orderBy($model->getTable() . ".created_at")->simplePaginate($pag);
+                        break;
+                }
             }
         }
 
