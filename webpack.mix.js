867654a138aad6f02e73e53842ffbf803a034e6d
Sina <sinabehzadi2014@gmail.com>
2022-09-03 17:47:23 +0400

    support vue

---
+++ webpack.mix.js
@@ -0,0 +1,36 @@
+let mix = require("laravel-mix");
+const path = require("path");
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+const time = new Date().getTime();
+
+mix
+  .js("resources/js/app.js", "public/js")
+  .setPublicPath("public")
+  .vue({ version: 2 })
+  //   .postCss("resources/css/app.css", "public")
+  //   .copy("public", "../nova-app/public/vendor/nova")
+  .webpackConfig({
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "resources/js/"),
+      },
+    },
+    // clean: {
+    //   keep(asset) {
+    //     if (/^(?!js\/).*/.test(asset)) return true;
+    //   },
+    // },
+    // chunkFilename: `js/[id]-${time}.js`,
+  })
+  .version();
