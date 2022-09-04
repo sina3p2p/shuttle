5f59a615ed0666a39979694fa37a3ec85fae5f2d
Sina <sinabehzadi2014@gmail.com>
2022-09-04 18:08:48 +0400

    webpack added

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,22 +15,31 @@ const path = require("path");
 const time = new Date().getTime();
 
 mix
-  .js("resources/js/app.js", "public/js")
+  .js("resources/js/app.js", "shuttle-vue")
   .setPublicPath("public")
   .vue({ version: 2 })
+  .extract([])
   //   .postCss("resources/css/app.css", "public")
   //   .copy("public", "../nova-app/public/vendor/nova")
   .webpackConfig({
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "resources/js/"),
-      },
-    },
+    // resolve: {
+    //   alias: {
+    //     "@": path.resolve(__dirname, "resources/js/"),
+    //   },
+    // },
     // clean: {
     //   keep(asset) {
     //     if (/^(?!js\/).*/.test(asset)) return true;
     //   },
     // },
     // chunkFilename: `js/[id]-${time}.js`,
+    output: {
+      // clean: {
+      //     keep(asset) {
+      //         if (/^(?!js\/).*/.test(asset)) return true;
+      //     },
+      // },
+      chunkFilename: `shuttle-vue/component/[id].js`,
+    },
   })
   .version();
